492c371ed2c95e5caae5a07651fcb2e03d3ee451
MrsFab09 <ytfabiskora07@gmail.com>
2023-12-19 22:23:31 +0100

    finish navbar

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+    <Image
+      className="rounded-full"
+      src="/images/placeholder.jpg"
+      width="30"
+      height="30"
+      alt="avatae"
+    />
+  );
+};
+export default Avatar;
